8e82c76e087f1d0a16ba6968eb6aca28d5c6a5d9
pan1dan <panonedan@yandex.ru>
2024-11-12 03:34:15 +0500

    2v

--- stats-service/stats-server/src/main/java/ru/practicum/StatsRepository.java
+++ stats-service/stats-server/src/main/java/ru/practicum/StatsRepository.java
@@ -3,14 +3,12 @@ package ru.practicum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import ru.practicum.dto.EndpointHit;
 import ru.practicum.dto.ViewStats;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Repository
 public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
     @Query("SELECT new ru.practicum.dto.ViewStats (e.app, e.uri, COUNT(DISTINCT e.ip)) " +
            "FROM EndpointHit e " +
