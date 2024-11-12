88f4426ff92fa6c4bcf9de36006eaecfd67d506d
pan1dan <panonedan@yandex.ru>
2024-11-13 01:43:11 +0500

    v2

--- stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -1,6 +1,6 @@
 package ru.practicum.dto;
 
-//import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -21,8 +21,7 @@ public class EndpointHitDto {
     @NotBlank
     @NotNull
     String ip;
-    @NotBlank
     @NotNull
-    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime timestamp;
 }
