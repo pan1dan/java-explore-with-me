33f8b5d22a1f711ecb90fe5ed81637fba0cbd649
pan1dan <panonedan@yandex.ru>
2024-11-12 20:17:09 +0500

    7v

--- main-service/src/main/java/ru/practicum/MainServer.java
+++ main-service/src/main/java/ru/practicum/MainServer.java
@@ -2,10 +2,8 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"ru.practicum", "ru.practicum.client"})
 public class MainServer {
     public static void main(String[] args) {
         SpringApplication.run(MainServer.class, args);
